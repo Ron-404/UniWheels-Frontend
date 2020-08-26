6b4377b0838060e9465b4f3a21ca339cd33d938f
Camu10 <manuel.murillo.1999@gmail.com>
2020-08-26 17:36:11 -0500

    Vista registrar usuario

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import DashBoardPasajero from './components/dashboard_pasajero/DashBoardPasajero';
 import DashBoardConductor from './components/dashboard_conductor/DashBoardConductor';
+import RegistrarUsuario from './components/Registrar_Usuario/RegistrarUsuario';
 import Home from './components/home/Home';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' component={Home} exact ></Route>
           <Route path='/dashboardPasajero' component={DashBoardPasajero} exact ></Route>
           <Route path='/dashboardConductor' component={DashBoardConductor} exact ></Route>
+          <Route path='/RegistrarUsuario' component={RegistrarUsuario} exact ></Route>
         </Switch>
       </BrowserRouter>
     </div>
