5b02702df4fb7d9f109e9ff43a656011b9c1dd7e
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-08-26 22:29:43 -0500

    Terminado Creo

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from '../../logo.png';
 import './Home.css';
+
 import bannerimg from './banner.jpg';
+import road from './road.jpg';
+import drive from './drive.jpg';
+import route from './route.png';
+import money from './money.png';
+import connect from './connect.png';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+
+
 class Home extends Component {
 
     constructor(props) {
@@ -17,29 +25,63 @@ class Home extends Component {
 
         return (
           <div >
-
-            <div id="banner">
-
-              <img id="bannerimg" src={bannerimg}/>
-              <header>
-                <div>
-                <h1> Uni-Wheels </h1>
-                </div>
-                <div>
-                  <img src={logo} id="logito" alt="logo" />
-                </div>
+            <section id="home">
+              <div id="banner">
+                <img id="bannerimg" src={drive}/>
+                <header>
+                  <div>
+                  <h1> Uni-Wheels </h1>
+                  </div>
+                  <div>
+                    <img src={logo} id="logito" alt="logo" />
+                  </div>
                     <div className="header-right">
-                     <a href="/">Inicio</a>
-                     <a href="#contact">Contacto</a>
+                     <a href="/">Login</a>
+                     <a href="/">Registro</a>
                      <a href="#about">Acerca</a>
                    </div>
-              </header>
-              <div id="acerca" className="centered">
-                  <p>LA MEJOR FORMA DE LLEGAR A LA UNIVERSIDAD </p>
-              </div>
-            </div>
+                </header>
+                <div id="slogan" className="centered">
+                    <p>LA MEJOR FORMA DE LLEGAR A LA UNIVERSIDAD </p>
+                </div>
+                  </div>
+              </section>
+              <section id="about">
+                <div id="info" className="gridcont">
+                   <Grid container spacing={3}>
+                     <Grid item xs={6}>
+                        <h2> ¿Quienes somos? </h2>
+                       <p>Somos una aplicación que te da la oportunidad de llegar a tu casa o a tu universidad fácil y comodamente. Interactuando con miembros de la comunidad universitaria de Bogotá, donde tu decides si quieres ser conductor o ser pasajero.</p>
+                     </Grid>
+                     <Grid item xs={6} className="cuadro">
+                       <img src={road}/>
+                     </Grid>
+                 </Grid>
+                </div>
+              </section>
+              <section id="ls">
+                  <div id="otros" className="gridcont">
+                     <Grid container spacing={3}>
+                       <Grid item xs>
+                          <h3> Muevete de forma segura y confiable </h3>
+                          <p> Tu conductor será un miembro de una universidad de la ciudad. </p>
+                          <img src={route} className="valores"/>
+                       </Grid>
+                       <Grid item xs>
+                          <h3> Ahorra Dinero </h3>
+                          <p> Pagaras un valor similar al del transporte público. </p>
+                            <img src={money} className="valores"/>
+                       </Grid>
+                       <Grid item xs>
+                          <h3> Conectate con mas Personas</h3>
+                          <p> Podrás conocer a muchas personas de la comunidad universitaria de Bogotá. </p>
+                          <img src={connect} className="valores"/>
+                       </Grid>
+                   </Grid>
+                  </div>
 
-
+              </section>
+              <a id="upbutton"  href="#"> <i className="fa fa-arrow-circle-up"></i></a>
 
           </div>
         )
