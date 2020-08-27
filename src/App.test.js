39447b174e0e1bf7c8664dae61245e1a2ec07db1
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-08-26 22:34:33 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import DashBoardPasajero from './components/dashboard_pasajero/DashBoardPasajero
 // Prueba Render Home
 test('render Home', () => {
   const { getByText } = render(<Home />);
-  const linkElement = getByText(/proyecto base uniWheels/i);
+  const linkElement = getByText(/¿Quienes somos?/i);
   expect(linkElement).toBeInTheDocument();
 });
 
