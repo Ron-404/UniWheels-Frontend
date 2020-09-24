9e1174f9c040346febc1a53404601ffc639d8b19
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-09-24 15:38:01 -0500

    Mapa de zonas y menu de zonas.

--- src/components/dashboard_conductor/OfrecerViaje.js
+++ src/components/dashboard_conductor/OfrecerViaje.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './OfrecerViaje.css';
 import {  withStyles,
-    TextField, 
-    MenuItem, 
-    Button, 
+    TextField,
+    MenuItem,
+    Button,
     Grid,
 } from '@material-ui/core';
 
@@ -25,6 +25,25 @@ const styles = theme => ({
         height: 25,
       },
 });
+
+const zonas= [
+  {id:"1", name: "1 Salidas Norte de Bogotá"},
+  {id:"2", name: "2 Suba Occidental"},
+  {id:"3", name: "3 Suba Oriental"},
+  {id:"4", name: "4 Usaquén Central"},
+  {id:"5", name: "5 Inicio Autopista Norte Oriental"},
+  {id:"6", name: "6 Calle 80 - Av 68"},
+  {id:"7", name: "7 Engativá"},
+  {id:"8", name: "8 Centro"},
+  {id:"9", name: "9 Fontibon"},
+  {id:"10", name: "10 Salitre - Teusaquillo"},
+  {id:"11", name: "11 Kennedy"},
+  {id:"12", name: "12 Bosa"},
+  {id:"13", name: "13 San Cristobal - Ciudad Bolivar"},
+  {id:"14", name: "14 Chía y alrededores"},
+  {id:"15", name: "15 Cajicá y Alrededores"},
+  {id:"16", name: "16 Cota y Alrededores"}
+]
 
 class OfrecerViaje extends React.Component {
 
@@ -74,10 +93,8 @@ class OfrecerViaje extends React.Component {
         document.body.classList.add('OfrecerViaje');
         return (
             <Grid container>
-                <Grid item xs={12} sm={6}>
-                            <div className="div-image-viaje OfrecerViaje">
-                                <img src={viaje} height="300" width="500" alt="img-registro"/>
-                            </div>
+                <Grid item xs={12} sm={6} id="zonamapa">
+                    <iframe id="mapazona" title="zonas" src="https://www.google.com/maps/d/embed?mid=1eCm1IraFBJkNpkpOQ-DnlR7ePFC1KbZT" width="640" height="480"></iframe>
                 </Grid>
                 <Grid item xs={12} sm={6}>
                             <form onSubmit={this.handleSubmit} className="form-OfrecerViaje">
@@ -88,29 +105,25 @@ class OfrecerViaje extends React.Component {
                                     <div className="text-form-cond OfrecerViaje">
                                         <TextField id="select" label="¿Donde estás?" select required fullWidth
                                             onChange={this.handleOrigenChange}>
-                                            <MenuItem value="usuario">Mi Casa</MenuItem>
-                                            <MenuItem value="conductor">ECI</MenuItem>
+                                            {zonas.map( (zona)=>(  <MenuItem value={zona.id}>{zona.name}</MenuItem>))}
                                         </TextField>
                                     </div>
                                     <br></br>
                                     <div className="text-form-cond OfrecerViaje">
                                         <TextField id="otlined-select" label="¿Para donde vás?" select required fullWidth
                                             onChange={this.handleDestinoChange}>
-                                            <MenuItem value="usuario">Mi casa</MenuItem>
-                                            <MenuItem value="conductor">ECI</MenuItem>
+                                            {zonas.map( (zona)=>(  <MenuItem value={zona.id}>{zona.name}</MenuItem>))}
                                         </TextField>
                                     </div>
                                     <br></br>
                                     <br></br>
                                     <div className="text-form-cond">
-                                        <TextField id="outlined-number" label="Precio" type="number" InputLabelProps={{ shrink: true,}}  
+                                        <TextField id="outlined-number" label="Precio" type="number" InputLabelProps={{ shrink: true,}}
                                             variant="outlined"  onChange={this.handlePrecioChange}  fullWidth   autoFocus
                                             required/>
                                     </div>
                                     <br></br>
                                 </div>
-                                <br></br>
-                                <br></br>
                                 <div>
                                     <Button type="submit" color="primary" variant="contained"  className="submit">
                                         Iniciar
