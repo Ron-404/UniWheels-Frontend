2132ea0aba83fb1ab14a8753e06b481c2f1bcad8
andresro30 <andresrochag15@hotmail.com>
2020-10-22 12:04:27 -0500

    change infoPerfil

--- src/components/Generales/infoPerfil.js
+++ src/components/Generales/infoPerfil.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-
+import Input from '@material-ui/core/Input';
 import ReactStars from "react-rating-stars-component";
+import Swal from 'sweetalert2';
 
 import { makeStyles} from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 import Button from '@material-ui/core/Button';
 
@@ -15,12 +17,27 @@ class InfoPerfil extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: false,
-            open:false,
-            user:this.props.user,
+            //name: false,
+            open: true,
+            edit: false,
+            //user:[],
+            name:'',
+            email:'',
+            rating:0,
+            password:'',
+            confirPass:'',
+            direction:'',
         }
         this.handleOpen = this.handleOpen.bind(this);
         this.handleClose = this.handleClose.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleRating = this.handleRating.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleConfirPass = this.handleConfirPass.bind(this);
+        this.handleDirection = this.handleDirection.bind(this);
+        this.handleSave = this.handleSave.bind(this);
         console.log("infoPerfil");
     }
 
@@ -32,14 +49,145 @@ class InfoPerfil extends Component {
         this.setState({ open: false });
     };
 
+    handleEdit(){
+      this.setState({ edit: !this.state.edit });
+    }
+
+    handleName(e){
+      this.setState({ name: e});
+    }
+
+    handleEmail(e){
+      this.setState({ email: e});
+    }
+
+    handleRating(e){
+      this.setState({ rating: e});
+    }
+
+    handlePassword(e){
+      this.setState({ password: e});
+    }
+
+    handleConfirPass(e){
+      this.setState({ confirPass: e});
+    }
+
+    handleDirection(e){
+      this.setState({ direction: e});
+    }
+
+    handleSave(){
+      var f = "@escuelaing.edu.co";
+
+      if (this.state.email === '' ||
+        this.state.user === '' ||
+        this.state.university === '' ||
+        this.state.address === '' ||
+        this.state.confirmPassword === '' ||
+        this.state.password === '') {
+        Swal.fire("Algún espacio esta vacio", "Por favor llene todos los campos", "error");
+      } 
+      else if (!this.state.email.includes(f)) {
+          Swal.fire("El correo no corresponde con uno institucional.", "Por favor ingrese un correo institucional.", "error");
+          return;
+      } 
+      else if (this.state.password !== this.state.confirmPassword) {
+          Swal.fire("Las contraseñas ingresadas no coinciden.", "Por favor ingrese de nuevo las contraseñas.", "error");
+          return;
+      } 
+        else {
+          //Conectar con la API updateUser
+          //Revisar conexión de RegistrarUsuario
+          Swal.fire(
+            'Cuenta creada satisfactoriamente!',
+            'Sera redireccionado a la pagina de inicio de sesion',
+            'success'
+          )
+      }
+    }
+
+    componentDidMount(){
+      //Traer info del api getUser
+      this.handleName("Orlando");
+      this.handleEmail("orlando@escuelaing.edu.co");
+      this.handleRating(2);
+    }
+
     render() {
+        const {classes} = this.props;
         return (
-            <TransitionsModal user={this.state.user}></TransitionsModal>
+          <div>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={this.state.open}
+            onClose={this.handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={this.state.open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">Nombre: </h2>
+                {!this.state.edit ? this.state.name : 
+                   <Input id="user" name="user" value={this.state.name} onChange={this.handleName} autoComplete="user" autoFocus /> }
+  
+                <h2 id="transition-modal-description">Correo: </h2>
+                  {!this.state.edit ? this.state.email :
+                    <Input id="email" name="email" value={this.state.email} onChange={this.handleEmail} autoComplete="email" autoFocus />}
+  
+                {this.state.edit ? 
+                  <div>
+                    <h2 id="transition-modal-description">Contraseña: </h2>
+                    <Input id="pass" type="password" name="pass" value={this.state.password} onChange={this.handlePassword} autoComplete="pass" autoFocus />
+                    <h2 id="transition-modal-description">Confirmar Contraseña: </h2>
+                    <Input id="confirmPass" type="password" name="confirmPass" value={this.state.confirmPass} onChange={this.handleConfirPass} autoComplete="confirmPass" autoFocus />
+                    <h2 id="transition-modal-description">Dirección: </h2>
+                    <Input id="direction" name="direction" value={this.state.direction} onChange={this.handleDirection} autoComplete="direction" autoFocus />
+                  </div>
+                  : null
+                }
+
+                {!this.state.edit ?  
+                  <div>
+                    <div >
+                      <h2 id="transition-modal-calificacion">Calificación: </h2>
+                      <ReactStars
+                          value={this.state.rating}
+                          size={24}
+                          color="#AFAFAF"
+                          activeColor="#ffd700"
+                          edit={false}
+                      />
+                    </div>
+                    <br></br>
+                    <Button color="primary" variant="contained"  className="submit" onClick={this.handleEdit}>
+                        Editar
+                    </Button> 
+                  </div> 
+                  : 
+                    <div>
+                      <br></br>
+                      <Button color="primary" variant="contained"  className="submit" onClick={this.handleSave}>
+                        Guardar
+                      </Button> 
+                    </div>
+                }
+                  
+  
+              </div>
+            </Fade>
+          </Modal>
+        </div>
         )
     }
 }
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
     modal: {
       display: 'flex',
       alignItems: 'center',
@@ -55,11 +203,13 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: "-30px",
         marginTop: "-5px"
     }
-  }));
+  });
 
+  /** 
 const TransitionsModal = ((props) => {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
+    const [edit, setEdit] = React.useState(false);
 
     const { user } = props;
   
@@ -67,8 +217,15 @@ const TransitionsModal = ((props) => {
       setOpen(false);
     };
 
+    const handleName = (e) => {
+      this.setState({
+        [user.name] : e.target.value
+      });
+    }
+
     const handleEdit = () => {
-        document.location.href="/ActualizarUsuario";
+        //document.location.href="/ActualizarUsuario";
+        setEdit(true);
       };
   
     return (
@@ -88,7 +245,9 @@ const TransitionsModal = ((props) => {
           <Fade in={open}>
             <div className={classes.paper}>
               <h2 id="transition-modal-title">Nombre: </h2>
-                {user.name}
+              {!edit ? user.name : 
+                 <Input id="user" name="user" value={user.name} onChange={handleName} autoComplete="user" autoFocus /> }
+
               <h2 id="transition-modal-description">Correo: </h2>
                 {user.email}
 
@@ -114,6 +273,6 @@ const TransitionsModal = ((props) => {
     );
   })
 
+*/
 
-
-export default InfoPerfil;
+export default withStyles(styles, { withTheme: true })(InfoPerfil);
